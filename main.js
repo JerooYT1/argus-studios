055a76a7bd54dedc1130123d7600272271aa7d26
JerooYT1 <103337913+JerooYT1@users.noreply.github.com>
2022-12-25 23:24:21 -0300

    Sisisoso
    Sososoosooss

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+    const parrafos = document.querySelectorAll('.descripcion')
+    let altura =  [];
+    let alturaMaxima = 0;
+
+    const aplicarAltura = (function aplicarAltura(){
+        parrafos.forEach(parrafos =>{
+            if (alturaMaxima) {
+                alturas.push(parrafo.clientHeight);
+            } else{
+                parrafo.style.height = alturaMaxima + ¨px¨;
+          }
+        });
+        return aplicarAltura;
+    })();
+    alturaMaxima =Math.max.apply(Math, alturas);
+    aplicarAltura();
+})
